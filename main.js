f120e45651091b2c43b183a1c476aa6fbbb8c12d
Sean Stewart <stewarse@gmail.com>
2021-05-23 17:16:15 -0700

    Updated code to include a clear button board as well as to set the color on a hover(mouseover) event

--- main.js
+++ main.js
@@ -1,14 +1,47 @@
 
 const container = document.querySelector('#container');
+const resetButton = document.querySelector('button#reset-board') 
 
 
-let num = 16
+setBoard()
 
-for (let i = 0; i < num; i++){
-    for(let j = 0; j < num; j++){
-    const newDiv = document.createElement('div');
-    newDiv.className = 'grid-item';
-    newDiv.id = `item-${i}`;
-    container.appendChild(newDiv);
+const items = document.querySelectorAll('.grid-item')
+
+items.forEach((item) => {
+    item.addEventListener('mouseover', changeColor);
+});
+
+/**********************************************************************************
+ * TO-DO Need to update to prompt user to determine how many pixels should be in the grid up to 100 x 100;
+***********************************************************************************/
+
+function changeColor( e ) {
+    let color = randomColor();
+    e.target.style.background = `hsl(${color}, 80%, 90%)`
+
+//Update code to adjust the saturation of the color (requires a new function based on rgb values)
+    console.log(e.target)
+}
+
+function setBoard() {
+    let num = 16
+
+    for (let i = 0; i < num; i++){
+        for(let j = 0; j < num; j++){
+        const newDiv = document.createElement('div');
+        newDiv.className = 'grid-item';
+        newDiv.id = `item-${i}`;
+        container.appendChild(newDiv);
+        }
     }
+};
+
+function clearBoard() {
+    items.forEach((item) => {
+        item.style.background = 'white';
+    });
+};
+
+function randomColor() {
+    return `${Math.floor(Math.random() * 360)}`
 }
