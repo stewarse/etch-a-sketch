31752d60bfe33d70974d21ff4402ff221fe310e4
Sean Stewart <stewarse@gmail.com>
2021-06-23 20:04:39 -0700

    Updating color picker to only happen when color is not selected

--- main.js
+++ main.js
@@ -3,27 +3,34 @@ const container = document.querySelector('#container');
 const resetButton = document.querySelector('button#reset-board') 
 
 
-setBoard();
-setEventListeners();
+createGrid();
+
 
 
 // items.forEach((item) => {
-//     item.addEventListener('mouseover', changeColor);
+//     item.addEventListener('mouseover', setColor);
 // });
 
 /**********************************************************************************
  * TO-DO Need to update to prompt user to determine how many pixels should be in the grid up to 100 x 100;
 ***********************************************************************************/
 
-function changeColor( e ) {
-    let color = randomColor();
-    e.target.style.background = `hsl(${color}, 80%, 90%)`
+function setColor( e ) {
+    //console.log(e.target.style)
+    if(this.style.background === ''){
+        let color = randomColor();
+        e.target.style.background = `hsl(${color}, 80%, 90%)`
+        //console.log('true')
+    } else {
+        //Determine the new color at 80% opacity
+
+    }
 
 //Update code to adjust the lightness of the color (requires a new function to determine new color via rgb values?)
-    console.log(e.target)
+    // console.log(e)
 }
 
-function setBoard(num = 16) {
+function createGrid(num = 16) {
 
     for (let i = 0; i < num; i++){
         for(let j = 0; j < num; j++){
@@ -36,24 +43,25 @@ function setBoard(num = 16) {
 
     container.style.gridTemplateColumns = `repeat(${num}, 1fr)`
     container.style.gridTemplateRows = `repeat(${num}, 1fr)`
+
+    setEventListeners();
 };
 
 function setEventListeners() {
     const items = document.querySelectorAll('.grid-item')
     
     items.forEach((item) => {
-        item.addEventListener('mouseover', changeColor);
+        item.addEventListener('mouseover', setColor);
     });
 }
 
 function resetBoard() {
-    clearBoard();
+    clearGrid();
     let gridSize = getGridSize();
-    setBoard(gridSize);
-    setEventListeners();
+    createGrid(gridSize);
 }
 
-function clearBoard() {
+function clearGrid() {
     while(container.firstChild) {
         container.removeChild(container.firstChild);
     }
